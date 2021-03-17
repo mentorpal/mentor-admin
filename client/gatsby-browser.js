ba18111a89e90b8684c2bcda86a9c7f0acd40a1a
Kayla Carr <whizbon@gmail.com>
2021-03-17 13:14:50 -0700

    Gatsby redirect test
    Record screen uses updated query instead of filtering manually

--- client/gatsby-browser.js
+++ client/gatsby-browser.js
@@ -18,3 +18,15 @@ export const wrapRootElement = ({ element }) => (
     </CookiesProvider>
   </MuiThemeProvider>
 );
+
+export const onRouteUpdate = ({ location, prevLocation }) => {
+  if (
+    typeof window !== "undefined" &&
+    !window.location.protocol.toLowerCase().startsWith("https")
+  ) {
+    const port = window.location.port ? `:${window.location.port}` : "";
+    const redirect = `https://${window.location.hostname}${port}${window.location.pathname}${window.location.search}`;
+    console.log(redirect);
+    window.location.href = redirect;
+  }
+};
