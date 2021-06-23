69d77140a1302dfeb90ed09da9c10e035892cfd4
aaronshiel <ashiel408@gmail.com>
2021-06-22 18:10:28 -0700

    cancelling status bug fix

--- client/src/components/record/uploading-list-item.tsx
+++ client/src/components/record/uploading-list-item.tsx
@@ -89,7 +89,7 @@ function UploadingListItem(props: {
             : jobTitle
         }
         secondary={
-          cancelling || cancelledAnswer ? (
+          cancelling || jobStatus === UploadStatus.CANCELLED ? (
             "Cancelling"
           ) : jobStatus === UploadStatus.TRANSCRIBE_FAILED ? (
             "Transcribe Failed"
