ef22193001be1cb3b4eebcdea637158a07941e4e
Luke Ostrander <lw04ostr@siena.edu>
2022-06-07 14:13:51 -0700

    minor changes

--- cypress/cypress/integration/index.spec.ts
+++ cypress/cypress/integration/index.spec.ts
@@ -115,26 +115,23 @@ describe("Index page", () => {
     cy.get("[data-cy=skip-action-button]").should("be.disabled");
   });
 
-  it("skip button cycles through properly", () => {
+  it.only("skip button cycles through properly", () => {
     cyMockDefault(cy, {
       mentor: clint10,
     });
     cy.visit("/");
 
     cy.get("[data-cy=recommended-action]").contains("Add a Thumbnail");
-
     cy.get("[data-cy=skip-action-button]").trigger("mouseover").click();
     cy.get("[data-cy=skip-action-button]").should("exist");
 
     cy.get("[data-cy=recommended-action]").contains(
       "Answer Leadership Questions"
     );
-
     cy.get("[data-cy=skip-action-button]").trigger("mouseover").click();
     cy.get("[data-cy=skip-action-button]").should("exist");
 
     cy.get("[data-cy=recommended-action]").contains("Add a Subject");
-
     cy.get("[data-cy=skip-action-button]").trigger("mouseover").click();
     cy.get("[data-cy=skip-action-button]").should("exist");
 
