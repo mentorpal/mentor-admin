2dbf9b4c3f73960f56d98c41b2cae7e74609092d
aaronshiel <ashiel408@gmail.com>
2022-06-24 15:40:12 -0700

    flaky setup apge fix

--- client/src/hooks/graphql/use-with-setup.tsx
+++ client/src/hooks/graphql/use-with-setup.tsx
@@ -250,7 +250,8 @@ export function useWithSetup(search?: { i?: string }): UseWithSetup {
     isEdited: isMentorEdited,
     isLoading: isMentorLoading,
     isSaving: isMentorSaving,
-    readyToDisplay: isConfigLoaded(),
+    readyToDisplay:
+      isConfigLoaded() && !isMentorLoading && mentor && !questionsLoading,
     error: mentorError,
     editMentor,
     saveMentor: saveMentorDetails,
