89ee682da8061f63071e42d6f2d864374080a765
Luke Ostrander <lw04ostr@siena.edu>
2022-06-24 14:34:27 -0700

    minor changes

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -1,12 +1,27 @@
-const path = require("path");
+// const path = require("path");
 
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
+// exports.onCreateWebpackConfig = ({ actions }) => {
+//   actions.setWebpackConfig({
+//     resolve: {
+//       alias: {
+//         "@": path.resolve(__dirname, "src"),
+//       },
+//       modules: [path.resolve(__dirname, "src"), "node_modules"],
+//     },
+//   });
+// };
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /bad-module/,
+            use: loaders.null(),
+          },
+        ],
       },
-      modules: [path.resolve(__dirname, "src"), "node_modules"],
-    },
-  });
+    });
+  }
 };
