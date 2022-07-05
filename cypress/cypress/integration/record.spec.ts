25b4014e9a4f008fa0464ec1e58bf60e6f6df4a6
Luke Ostrander <lw04ostr@siena.edu>
2022-07-05 15:39:12 -0700

    discovered that mediapipe selfie segmentation is a better approach

--- cypress/cypress/integration/record.spec.ts
+++ cypress/cypress/integration/record.spec.ts
@@ -24,6 +24,7 @@ import {
   completeSubjectQuestion,
   updateMentorAnswer,
 } from "../support/helpers";
+
 export function taskListBuild(progressForAllTasks) {
   return {
     trimUploadTask: {
@@ -81,12 +82,26 @@ const chatMentor: Mentor = completeMentor({
       categories: [{ id: "cat", name: "cat", description: "cat" }],
       questions: [
         completeSubjectQuestion({
-          question: { _id: "A1_1_1" },
-          category: { id: "cat" },
+          question: {
+            _id: "A1_1_1",
+            clientId: "C1_1_1",
+            question: "Question 1",
+            name: null,
+            type: QuestionType.QUESTION,
+            paraphrases: [],
+          },
+          category: { id: "cat", name: "cat", description: "cat" },
           topics: [],
         }),
         completeSubjectQuestion({
-          question: { _id: "A2_1_1" },
+          question: {
+            _id: "A2_1_1",
+            clientId: "C2_1_1",
+            question: "Question 2",
+            name: null,
+            type: QuestionType.QUESTION,
+            paraphrases: [],
+          },
         }),
       ],
     }),
@@ -94,16 +109,44 @@ const chatMentor: Mentor = completeMentor({
       _id: "idle_and_initial_recordings",
       questions: [
         completeSubjectQuestion({
-          question: { _id: "A3_1_1" },
+          question: {
+            _id: "A3_1_1",
+            clientId: "C3_1_1",
+            question: "Question 3",
+            name: null,
+            type: QuestionType.UTTERANCE,
+            paraphrases: [],
+          },
         }),
         completeSubjectQuestion({
-          question: { _id: "A4_1_1" },
+          question: {
+            _id: "A4_1_1",
+            clientId: "C4_1_1",
+            question: "Question 4",
+            name: null,
+            type: QuestionType.UTTERANCE,
+            paraphrases: [],
+          },
         }),
         completeSubjectQuestion({
-          question: { _id: "A5_1_1" },
+          question: {
+            _id: "A5_1_1",
+            clientId: "C5_1_1",
+            question: "Question 5",
+            name: null,
+            type: QuestionType.UTTERANCE,
+            paraphrases: [],
+          },
         }),
         completeSubjectQuestion({
-          question: { _id: "A6_1_1" },
+          question: {
+            _id: "A6_1_1",
+            clientId: "C6_1_1",
+            question: "Question 6",
+            name: null,
+            type: QuestionType.QUESTION,
+            paraphrases: [],
+          },
         }),
       ],
     }),
@@ -111,33 +154,68 @@ const chatMentor: Mentor = completeMentor({
   answers: [
     {
       _id: "A1_1_1",
-      question: { _id: "A1_1_1" },
+      question: {
+        _id: "A1_1_1",
+        clientId: "C1_1_1",
+        question: "Question 1",
+        name: null,
+        type: QuestionType.QUESTION,
+        paraphrases: [],
+      },
       transcript:
         "My name is Clint Anderson and I'm a Nuclear Electrician's Mate",
       status: Status.COMPLETE,
     },
     {
       _id: "A2_1_1",
-      question: { _id: "A2_1_1" },
+      question: {
+        _id: "A2_1_1",
+        clientId: "C2_1_1",
+        question: "Question 2",
+        name: null,
+        type: QuestionType.QUESTION,
+        paraphrases: [],
+      },
       transcript: "",
       status: Status.INCOMPLETE,
     },
     {
       _id: "A3_1_1",
-      question: { _id: "A3_1_1" },
+      question: {
+        _id: "A3_1_1",
+        clientId: "C3_1_1",
+        question: "Question 3",
+        name: null,
+        type: QuestionType.UTTERANCE,
+        paraphrases: [],
+      },
       transcript: "",
       status: Status.INCOMPLETE,
     },
     {
       _id: "A4_1_1",
-      question: { _id: "A4_1_1" },
+      question: {
+        _id: "A4_1_1",
+        clientId: "C4_1_1",
+        question: "Question 4",
+        name: null,
+        type: QuestionType.UTTERANCE,
+        paraphrases: [],
+      },
       transcript:
         "My name is Clint Anderson and I'm a Nuclear Electrician's Mate",
       status: Status.COMPLETE,
     },
     {
       _id: "A5_1_1",
-      question: { _id: "A5_1_1" },
+      question: {
+        _id: "A5_1_1",
+        clientId: "C5_1_1",
+        question: "Question 5",
+        name: null,
+        type: QuestionType.UTTERANCE,
+        paraphrases: [],
+      },
       transcript: "",
       status: Status.INCOMPLETE,
     },
@@ -183,41 +261,75 @@ const videoMentor: Mentor = completeMentor({
   answers: [
     {
       _id: "A1_1_1",
-      question: { _id: "A1_1_1", question: "Who are you and what do you do?" },
+      question: {
+        _id: "A1_1_1",
+        clientId: "C1_1_1",
+        name: "A1_1_1",
+        type: QuestionType.QUESTION,
+        paraphrases: [],
+        question: "Who are you and what do you do?",
+      },
       transcript: "",
       status: Status.INCOMPLETE,
     },
     {
       _id: "A2_1_1",
-      question: { _id: "A2_1_1", question: "How old are you now?" },
-      transcript: "I'm 37 years old",
-      webMedia: {
-        type: MediaType.VIDEO,
-        tag: "web",
-        url: "A2_1_1.mp4",
+      question: {
+        _id: "A2_1_1",
+        clientId: "C2_1_1",
+        name: "A2_1_1",
+        type: QuestionType.QUESTION,
+        paraphrases: [],
+        question: "How old are you now?",
       },
+      transcript: "I'm 37 years old",
+      media: [
+        {
+          type: MediaType.VIDEO,
+          tag: "web",
+          url: "A2_1_1.mp4",
+        },
+      ],
       status: Status.COMPLETE,
     },
     {
       _id: "A3_1_1",
-      question: { _id: "A3_1_1", question: "Where do you live?" },
-      transcript: "In Howard City, Michigan",
-      webMedia: {
-        type: MediaType.VIDEO,
-        tag: "web",
-        url: "A3_1_1.mp4",
+      question: {
+        _id: "A3_1_1",
+        clientId: "C3_1_1",
+        name: "A3_1_1",
+        type: QuestionType.UTTERANCE,
+        paraphrases: [],
+        question: "Where do you live?",
       },
+      transcript: "In Howard City, Michigan",
+      media: [
+        {
+          type: MediaType.VIDEO,
+          tag: "web",
+          url: "A3_1_1.mp4",
+        },
+      ],
       status: Status.COMPLETE,
     },
     {
       _id: "A4_1_1",
-      question: { _id: "A4_1_1", question: "Record an idle video" },
-      transcript: "",
-      webMedia: {
-        type: MediaType.VIDEO,
-        tag: "web",
-        url: "A3_1_1.mp4",
+      question: {
+        _id: "A4_1_1",
+        clientId: "C4_1_1",
+        name: "A4_1_1",
+        type: QuestionType.UTTERANCE,
+        paraphrases: [],
+        question: "How old are you now?",
       },
+      transcript: "",
+      media: [
+        {
+          type: MediaType.VIDEO,
+          tag: "web",
+          url: "A4_1_1.mp4",
+        },
+      ],
       status: Status.COMPLETE,
     },
   ],
@@ -227,11 +339,25 @@ const videoMentor: Mentor = completeMentor({
       name: "Subject 1",
       questions: [
         completeSubjectQuestion({
-          question: { _id: "A1_1_1" },
+          question: {
+            _id: "A1_1_1",
+            clientId: "C1_1_1",
+            question: "Question 1",
+            name: null,
+            type: QuestionType.QUESTION,
+            paraphrases: [],
+          },
           category: { id: "cat", name: "cat", description: "cat" },
         }),
         completeSubjectQuestion({
-          question: { _id: "A2_1_1" },
+          question: {
+            _id: "A2_1_1",
+            clientId: "C2_1_1",
+            question: "Question 2",
+            name: null,
+            type: QuestionType.QUESTION,
+            paraphrases: [],
+          },
           category: { id: "cat", name: "cat", description: "cat" },
         }),
       ],
@@ -242,10 +368,24 @@ const videoMentor: Mentor = completeMentor({
       name: "Subject 2",
       questions: [
         completeSubjectQuestion({
-          question: { _id: "A3_1_1" },
+          question: {
+            _id: "A3_1_1",
+            clientId: "C3_1_1",
+            question: "Question 3",
+            name: null,
+            type: QuestionType.UTTERANCE,
+            paraphrases: [],
+          },
         }),
         completeSubjectQuestion({
-          question: { _id: "A4_1_1" },
+          question: {
+            _id: "A4_1_1",
+            clientId: "C4_1_1",
+            question: "Question 4",
+            name: null,
+            type: QuestionType.UTTERANCE,
+            paraphrases: [],
+          },
         }),
       ],
     }),
@@ -930,9 +1070,9 @@ describe("Record", () => {
     cy.get("[data-cy=back-btn]").trigger("mouseover").click();
   });
 
-  it("upload button changes to trim when trim is edited", () => {
+  it("upload button does not change to trim when trim is not editable", () => {
     cyMockDefault(cy, {
-      mentor: [videoMentor],
+      mentor: { ...videoMentor, isDirty: true },
       questions: videoQuestions,
       gqlQueries: [
         mockGQL("UploadTaskDelete", { me: { uploadTaskDelete: true } }),
@@ -943,16 +1083,14 @@ describe("Record", () => {
       ],
     });
     cy.visit("/record?videoId=A2_1_1");
-    cy.get("[data-cy=upload-video]").should("be.visible");
+    cy.get("[data-cy=upload-video]").should("be.hidden");
     cy.get("[data-cy=upload-video]").should("be.disabled");
     cy.get("[data-cy=upload-video]").should("have.text", "Upload Video");
     // edit slider
-    cy.get("[data-cy=slider]").within(($slider) => {
-      cy.get(".MuiSlider-thumb").first().type("{rightarrow}");
-    });
-    cy.get("[data-cy=upload-video]").should("be.visible");
-    cy.get("[data-cy=upload-video]").should("not.be.disabled");
-    cy.get("[data-cy=upload-video]").should("have.text", "Trim Video");
+    cy.get("[data-cy=slider]").should("be.hidden");
+    cy.get("[data-cy=upload-video]").should("be.hidden");
+    cy.get("[data-cy=upload-video]").should("be.disabled");
+    cy.get("[data-cy=upload-video]").should("have.text", "Upload Video");
   });
 
   it("the upload card corresponding to current question should be highlighted", () => {
@@ -1661,7 +1799,7 @@ describe("Record", () => {
     cy.get("[data-cy=video-player]").should("not.exist");
   });
 
-  it("shows video recorder if mentor type is VIDEO and no video", () => {
+  it("shows video recorder if mentor type is VIDEO and has no video", () => {
     cyMockDefault(cy, {
       mentor: [videoMentor],
       questions: videoQuestions,
@@ -1685,7 +1823,7 @@ describe("Record", () => {
     cy.get("[data-cy=upload-video]").should("be.hidden");
   });
 
-  it("shows video player if mentor type is VIDEO and has video", () => {
+  it("Does not show video player if mentor type is VIDEO and has no video", () => {
     cyMockDefault(cy, {
       mentor: [videoMentor],
       questions: videoQuestions,
@@ -1699,18 +1837,18 @@ describe("Record", () => {
     });
     cy.visit("/record?videoId=A2_1_1");
     cy.get("[data-cy=video-recorder]").should("exist");
-    // video recorder hidden
-    cy.get("[data-cy=video-recorder]").should("be.hidden");
-    cy.get("[data-cy=upload-file]").should("be.hidden");
-    // video player showing
-    cy.get("[data-cy=video-player]").should("be.visible");
-    cy.get("[data-cy=rerecord-video]").should("be.visible");
-    cy.get("[data-cy=upload-video]").should("be.visible");
-    // editing hidden
+
+    cy.get("[data-cy=video-recorder]").should("be.visible");
+    cy.get("[data-cy=upload-file]").should("be.visible");
+
+    cy.get("[data-cy=video-player]").should("be.hidden");
+    cy.get("[data-cy=rerecord-video]").should("be.hidden");
+    cy.get("[data-cy=upload-video]").should("be.hidden");
+
     cy.get("[data-cy=upload-video]").should("be.disabled");
-    cy.get("[data-cy=slider]").should("not.be.hidden");
-    // can re-record video
-    cy.get("[data-cy=rerecord-video]").trigger("mouseover").click();
+    cy.get("[data-cy=slider]").should("be.hidden");
+
+    cy.get("[data-cy=rerecord-video]").should("be.hidden");
     cy.get("[data-cy=video-recorder]").should("be.visible");
     cy.get("[data-cy=upload-file]").should("be.visible");
     cy.get("[data-cy=video-player]").should("be.hidden");
@@ -1719,22 +1857,7 @@ describe("Record", () => {
     cy.get("[data-cy=upload-video]").should("be.hidden");
   });
 
-  it.only("shows video player if mentor type is VIDEO and has video", () => {
-    cyMockDefault(cy, {
-      mentor: [videoMentor],
-      questions: videoQuestions,
-      gqlQueries: [
-        mockGQL("UploadTaskDelete", { me: { uploadTaskDelete: true } }),
-        mockGQL("UpdateAnswer", { me: { updateAnswer: true } }),
-        mockGQL("UpdateQuestion", { me: { updateQuestion: true } }),
-        mockGQL("ImportTask", { importTask: null }),
-        mockGQL("FetchUploadTasks", [{ me: { uploadTasks: [] } }]),
-      ],
-    });
-    cy.visit("/record?videoId=A2_1_1");
-  });
-
-  it("download video button only visible when video is present", () => {
+  it.only("download video button only visible when video is present", () => {
     cyMockDefault(cy, {
       mentor: [
         videoMentor,
