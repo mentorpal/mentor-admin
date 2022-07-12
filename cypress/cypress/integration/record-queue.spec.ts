7a82bcbb960bf87008b945737fa27202782638f7
kassgtz <kassgtz27@gmail.com>
2022-07-12 11:19:38 -0700

    record tests

--- cypress/cypress/integration/record-queue.spec.ts
+++ cypress/cypress/integration/record-queue.spec.ts
@@ -9,10 +9,8 @@ import { feedback as userQuestions } from "../fixtures/feedback/feedback";
 import mentor from "../fixtures/mentor/clint_new";
 import clint from "../fixtures/mentor/clint_home";
 import questions from "../fixtures/questions";
-import {
-  QuestionType,
-  Status,
-} from "../support/types";
+import { QuestionType, Status } from "../support/types";
+import { cli } from "cypress";
 
 describe("Mentor Record Queue", () => {
   it("Modal shows correct data", () => {
@@ -58,222 +56,157 @@ describe("Mentor Record Queue", () => {
     cy.get("[data-cy=Category-option-category3]").should("be.visible");
     cy.get("[data-cy=Category-option-category1]").click();
 
-<<<<<<< HEAD
-      cy.get("[data-cy=topic-selector]").click();
-      cy.get("[data-cy=Topic-option-back-topic1-id]").should("be.visible");
-      cy.get("[data-cy=Topic-option-back-topic2-id]").should("be.visible");
-    });
-    it.only("New custom question shows up on queue card", () => {
-      cySetup(cy);
-      cyMockDefault(cy, {
-        mentor: {...clint, answers: [...clint.answers, {
-          // Add answer with question document here
-          
-          //Answer: [
-              _id: "A1_1_2",
-              question: {
-                _id: "A1_1_2",
-                clientId: "C_A1_1_1",
-                question: "Custom Question?",
-                type: QuestionType.QUESTION,
-                name: null,
-                paraphrases: [],
-              },
-              transcript:
-                "",
-              status: Status.COMPLETE,
-            //],
-
-
-
-        }]},
-        questions: [...questions, {
-          // Add question document here
-          question: [
-            "A1_1_2",
-            "Custom Question?",
-            "QUESTION",
-            "",
-            clint._id,
-            [],
-            clint._id,
-          ]
-        }],
-        gqlQueries: [
-          mockGQL("FetchUploadTasks", [{ me: { uploadTasks: [] } }]),
-          mockGQL("UserQuestions", userQuestions),
-          mockGQL("ImportTask", { importTask: null }),
-          mockGQL("SubjectAddOrUpdateQuestions", {
-            me: {
-              subjectAddOrUpdateQuestions: [
-                "background",
-                {
-                  question: [
-                    "A1_1_2",
-                    "Custom Question?",
-                    "QUESTION",
-                    "",
-                    clint._id,
-                    [],
-                    clint._id,
-                  ],
-                  topics: [],
-                  category: "category",
-                },
-              ],
-            },
-          }),
-          mockGQL("AddQuestionToRecordQueue", {
-            me: {
-              addQuestionToRecordQueue: ["A1_1_2"],
-            },
-          }),
-          mockGQL("FetchMentorRecordQueue",
-          [
-            {
-              me: {
-                mentorRecordQueue: [],
-              },
-            },
-            {
-              me: {
-                mentorRecordQueue: [
-                  "A1_1_2"
-                ],
-              },
-            },
-          ]
-        ),
-          mockGQL("UserQuestionSetAnswer", {})
-        ],
-      });
-      cy.visit("/feedback");
-      cy.get("[data-cy=queue-btn]").click();
-      cy.get("[data-cy=subject-drop-down]").click();
-      cy.get("[data-cy=Subject-option-background]").click();
-      cy.get("[data-cy=category-drop-down]").click();
-      cy.get("[data-cy=Category-option-category]").click();
-      cy.get("[data-cy=modal-OK-btn]").click();
-      cy.visit("/");
-      cy.location("pathname").then(($el) => {
-        assert($el.replace("/admin", ""), "/");
-      });
-      cy.get("[data-cy=setup-no]").click();
-      cy.get("[data-cy=queue-block]").should("exist");
-      cy.get("[data-cy=queue-expand-btn]").click();
-    });
-  });
-
-  describe("Home Page", () => {
-    it("Queue card is accessible throughout home page", () => {
-      cyMockDefault(cy, {
-        mentor: clint,
-        gqlQueries: [
-          mockGQL("ImportTask", { importTask: null }),
-          mockGQL("FetchUploadTasks", [{ me: { uploadTasks: [] } }]),
-          mockGQL("FetchMentorRecordQueue", {
-            me: {
-              mentorRecordQueue: ["A2_1_1", "A4_1_1"],
-            },
-          }),
-        ],
-      });
-      cy.visit("/");
-      cy.location("pathname").then(($el) => {
-        assert($el.replace("/admin", ""), "/");
-      });
-      cy.get("[data-cy=setup-no]").click();
-      cy.get("[data-cy=queue-block]").should("exist");
-      cy.get("[data-cy=select-subject]").click(); 
-      cy.get("[data-cy=select-background]").click();
-      cy.get("[data-cy=queue-block]").should("exist");
-      cy.get("[data-cy=select-subject]").click(); 
-      cy.get("[data-cy=select-repeat_after_me]").click();
-      cy.get("[data-cy=queue-block]").should("exist");
-    });
-    it("Can record all queued questions", () => {
-      cyMockDefault(cy, {
-        mentor: clint,
-        gqlQueries: [
-          mockGQL("ImportTask", { importTask: null }),
-          mockGQL("FetchUploadTasks", [{ me: { uploadTasks: [] } }]),
-          mockGQL("FetchMentorRecordQueue", {
-            me: {
-              mentorRecordQueue: ["A2_1_1", "A4_1_1"],
-            },
-          }),
-        ],
-      });
-      cy.visit("/");
-      cy.location("pathname").then(($el) => {
-        assert($el.replace("/admin", ""), "/");
-      });
-      cy.get("[data-cy=setup-no]").click();
-      cy.get("[data-cy=queue-expand-btn]").click();
-      cy.get("[data-cy=record-all-queue]").click();
-    cy.location("search").should(
-      "equal",
-      "?videoId=A2_1_1&videoId=A4_1_1"
-    );
-    });
-    it("Can record a single queue question", () => {
-      cyMockDefault(cy, {
-        mentor: clint,
-        gqlQueries: [
-          mockGQL("ImportTask", { importTask: null }),
-          mockGQL("FetchUploadTasks", [{ me: { uploadTasks: [] } }]),
-          mockGQL("FetchMentorRecordQueue", {
-            me: {
-              mentorRecordQueue: ["A2_1_1", "A4_1_1"],
-            },
-          }),
-        ],
-      });
-      cy.visit("/");
-      cy.location("pathname").then(($el) => {
-        assert($el.replace("/admin", ""), "/");
-      });
-      cy.get("[data-cy=setup-no]").click();
-      cy.get("[data-cy=queue-expand-btn]").click();
-      cy.get("[data-cy=record-one-0]").click();
-    cy.location("search").should(
-      "equal",
-      "?videoId=A2_1_1&back=%2F"
-    );
-=======
     cy.get("[data-cy=topic-selector]").click();
     cy.get("[data-cy=Topic-option-back-topic1-id]").should("be.visible");
     cy.get("[data-cy=Topic-option-back-topic2-id]").should("be.visible");
   });
-  it("Modal creates new question", () => {
+  it.only("New custom question shows up on queue card", () => {
     cySetup(cy);
     cyMockDefault(cy, {
-      mentor,
+      mentor: {
+        ...clint,
+        answers: [
+          ...clint.answers,
+          {
+            // Add answer with question document here
+          },
+        ],
+      },
       gqlQueries: [
+        mockGQL("FetchUploadTasks", [{ me: { uploadTasks: [] } }]),
         mockGQL("UserQuestions", userQuestions),
         mockGQL("ImportTask", { importTask: null }),
-        mockGQL("FetchMentorRecordQueue", {
-          me: {
-            mentorRecordQueue: [],
-          },
-        }),
         mockGQL("SubjectAddOrUpdateQuestions", {
-          me: { subjectAddOrUpdateQuestions: {} },
+          me: {
+            subjectAddOrUpdateQuestions: [
+              "background",
+              {
+                question: [
+                  "A1_1_2",
+                  "Custom Question?",
+                  "QUESTION",
+                  "",
+                  clint._id,
+                  [],
+                  clint._id,
+                ],
+                topics: [],
+                category: "category",
+              },
+            ],
+          },
         }),
         mockGQL("AddQuestionToRecordQueue", {
           me: {
-            addQuestionToRecordQueue: [],
+            addQuestionToRecordQueue: ["A1_1_2"],
           },
         }),
-        mockGQL("RemoveQuestionFromRecordQueue", {
-          me: {
-            removeQuestionFromRecordQueue: [],
+        mockGQL("FetchMentorRecordQueue", [
+          {
+            me: {
+              mentorRecordQueue: [],
+            },
           },
-        }),
+          {
+            me: {
+              mentorRecordQueue: ["A1_1_2"],
+            },
+          },
+        ]),
         mockGQL("UserQuestionSetAnswer", {}),
       ],
->>>>>>> UserFeedback
     });
+    cy.visit("/feedback");
+    cy.get("[data-cy=queue-btn]").click();
+    cy.get("[data-cy=subject-drop-down]").click();
+    cy.get("[data-cy=Subject-option-background]").click();
+    cy.get("[data-cy=category-drop-down]").click();
+    cy.get("[data-cy=Category-option-category]").click();
+    cy.get("[data-cy=modal-OK-btn]").click();
+    console.log();
+    /*
+      cy.visit("/");
+      cy.location("pathname").then(($el) => {
+        assert($el.replace("/admin", ""), "/");
+      });
+      cy.get("[data-cy=setup-no]").click();
+      cy.get("[data-cy=queue-block]").should("exist");
+      cy.get("[data-cy=queue-expand-btn]").click();
+      */
+  });
+});
+
+describe("Home Page", () => {
+  it("Queue card is accessible throughout home page", () => {
+    cyMockDefault(cy, {
+      mentor: clint,
+      gqlQueries: [
+        mockGQL("ImportTask", { importTask: null }),
+        mockGQL("FetchUploadTasks", [{ me: { uploadTasks: [] } }]),
+        mockGQL("FetchMentorRecordQueue", {
+          me: {
+            mentorRecordQueue: ["A2_1_1", "A4_1_1"],
+          },
+        }),
+      ],
+    });
+    cy.visit("/");
+    cy.location("pathname").then(($el) => {
+      assert($el.replace("/admin", ""), "/");
+    });
+    cy.get("[data-cy=setup-no]").click();
+    cy.get("[data-cy=queue-block]").should("exist");
+    cy.get("[data-cy=select-subject]").click();
+    cy.get("[data-cy=select-background]").click();
+    cy.get("[data-cy=queue-block]").should("exist");
+    cy.get("[data-cy=select-subject]").click();
+    cy.get("[data-cy=select-repeat_after_me]").click();
+    cy.get("[data-cy=queue-block]").should("exist");
+  });
+  it("Can record all queued questions", () => {
+    cyMockDefault(cy, {
+      mentor: clint,
+      gqlQueries: [
+        mockGQL("ImportTask", { importTask: null }),
+        mockGQL("FetchUploadTasks", [{ me: { uploadTasks: [] } }]),
+        mockGQL("FetchMentorRecordQueue", {
+          me: {
+            mentorRecordQueue: ["A2_1_1", "A4_1_1"],
+          },
+        }),
+      ],
+    });
+    cy.visit("/");
+    cy.location("pathname").then(($el) => {
+      assert($el.replace("/admin", ""), "/");
+    });
+    cy.get("[data-cy=setup-no]").click();
+    cy.get("[data-cy=queue-expand-btn]").click();
+    cy.get("[data-cy=record-all-queue]").click();
+    cy.location("search").should("equal", "?videoId=A2_1_1&videoId=A4_1_1");
+  });
+  it("Can record a single queue question", () => {
+    cyMockDefault(cy, {
+      mentor: clint,
+      gqlQueries: [
+        mockGQL("ImportTask", { importTask: null }),
+        mockGQL("FetchUploadTasks", [{ me: { uploadTasks: [] } }]),
+        mockGQL("FetchMentorRecordQueue", {
+          me: {
+            mentorRecordQueue: ["A2_1_1", "A4_1_1"],
+          },
+        }),
+      ],
+    });
+    cy.visit("/");
+    cy.location("pathname").then(($el) => {
+      assert($el.replace("/admin", ""), "/");
+    });
+    cy.get("[data-cy=setup-no]").click();
+    cy.get("[data-cy=queue-expand-btn]").click();
+    cy.get("[data-cy=record-one-0]").click();
+    cy.location("search").should("equal", "?videoId=A2_1_1&back=%2F");
+    /*
     cy.visit("/feedback");
     cy.get("[data-cy=queue-btn]").click();
     cy.get("[data-cy=subject-drop-down]").click();
@@ -283,5 +216,6 @@ describe("Mentor Record Queue", () => {
     cy.get("[data-cy=topic-selector]").click();
     cy.get("[data-cy=Topic-option-back-topic2-id]").click();
     cy.get("[data-cy=modal-OK-btn]").click();
+    */
   });
 });
