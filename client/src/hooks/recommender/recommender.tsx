df7e38c8573e1ee1138fda09bc39f06e2dd2dffe
Megan-J <67922354+Megan-J@users.noreply.github.com>
2022-07-15 16:23:45 -0700

    update

--- client/src/hooks/recommender/recommender.tsx
+++ client/src/hooks/recommender/recommender.tsx
@@ -5,40 +5,153 @@ Permission to use, copy, modify, and distribute this software and its documentat
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
 
-import { useState } from "react";
 
-export interface Recommendation {
-  buttonText: string;
-  icon: JSX.Element;
-  type: string;
-  // params
-  // attributes
+import { Answer, Mentor, MentorType, Status, UtteranceName } from "types";
+
+
+interface RecommenderState {
+  mentorId: string;
+  idle?: Answer;
+  intro?: Answer;
+  offTopic?: Answer;
+  introIncomplete: boolean;
+  idleIncomplete: boolean;
+  isVideo: boolean;
+  offTopicIncomplete: boolean;
+  hasThumbnail: boolean;
+  isDirty: boolean;
+  //categories: Category[];
+  //incompleteRequirement?: Category;
+  //firstIncomplete?: Category;
+  completedAnswers: number;
+  totalAnswers: number;
+  neverBuilt: boolean;
+  builttNeverPreview: boolean;
+  needSubject: boolean;
 }
 
-// These work hand in hand with RecommenderState
-export interface ProductionRule {
-  // any since compiler doesn't know what RecommenderStateT is.
-  condition: (recState: any) => boolean;
-  action: () => void;
-}
+/*****************
+RECOMMENDER CLASS
+*****************/
+class Recommender<RecommenderState>{
+  //all properties should be intialized in the constructor
+  recState: RecommenderState;
+  phases: Phase[];
+  
 
-/**
- * Confirm with Ben: The focus of this Recommender is not to calculate the conditions or state itself,
- * but instead it takes provided state, conditions, and weight, and use those to calculate
- * the next recommendation.
- * @param defaultState
- */
-export function recommender<RecommenderStateT>(
-  defaultState: RecommenderStateT
-) {
-  const [recommenderState, setRecommenderState] =
-    useState<RecommenderStateT>(defaultState);
-
-  function updateFromToken(token: RecommenderStateT) {
-    setRecommenderState(token);
+  public Recommender(recState: RecommenderState, phases: Phase[]){
+    this.recState = recState;
+    this.phases = phases;
   }
 
-  function getRecommendations(): Recommendation[] {
-    return [];
+  public getRecommendations(){
+    for(/*phase in this.phases */){
+      if(phase.isActive(this.recState)){
+        return phase.getRecommendations(this.recState);
+      }
+    }
+    return[]
   }
 }
+
+/**********
+PHASE CLASS
+***********/
+class Phase{
+
+  productionRoles = //another thing
+  activeCondition = //something
+
+  public Phase(activeCondition: (recState)=> boolean, productionRoles:ProductionRoles[]){
+    this.productionRoles = productionRoles;
+    this.activeCondition = activeCondition;
+  }
+
+  public isActive(recState: RecommenderState){
+    return this.activeCondition(recState);
+  }
+
+  public getRecommendations(){
+    //for productionRole in self.ProductionRoles:
+    // 			if productionRole.isActive(recState):
+    // 				return productionRole.getRecommendations(recState)
+    // 		return []
+  }
+}
+
+/*********************
+PRODUCTION ROLES CLASS
+*********************/
+class ProductionRoles{
+
+
+  public ProductionRoles(activeCondition: (recState)=> boolean, actionRecommendations: Recommendation[]){
+    this.activeCondition = activeCondition;
+    this.actionRecommendations = actionRecommendations;
+  }
+
+  public isActive(recState: RecommenderState){
+    return this.activeCondition(recState)
+  }
+
+  public getRecommendations(recState: RecommenderState){
+    return this.actionRecommendations
+  }
+}
+
+/********************
+RECOMMENDATION CLASS
+********************/
+class Recommendation{
+  public Recommendation(actions:any, attributes:any, buttonText:any, icon: any, type:any){
+
+  }
+}
+
+// class Recommender<RecommenderState>{
+// 	public Recommender(recState: RecommenderState, phases: Phase[]):
+// 		self.recState = recState
+// 		self.phases = phases
+	
+// 	public getRecommendations():
+// 		for phase in self.phases: # check phases in the order that they are provided
+// 			if phase.isActive(self.recState):
+// 				return phase.getRecommendations(self.recState)
+// 		return []
+// }
+ 
+// class Phase{
+// 	public Phase(activeCondition: (recState)=>boolean, productionRoles: ProductionRoles[]):
+// 		self.productionRoles = productionRoles
+// 		self.activeCondition = activeCondition
+ 
+// 	public isActive(recState: RecommenderState):
+// 		return self.activeCondition(recState)
+	
+// 	public getRecommendations(recState: RecommenderState):
+// 		for productionRole in self.ProductionRoles:
+// 			if productionRole.isActive(recState):
+// 				return productionRole.getRecommendations(recState)
+// 		return []
+// }
+ 
+// class ProductionRoles{
+// 	public ProductionRoles(activeCondition: (recState)=>boolean, actionRecommendations: Recommendation[]):
+// 		self.activeCondition = activeCondition
+// 		self.actionRecommendations = actionRecommendations
+ 
+// 	public isActive(recState: RecommenderState):
+// 		return self.activeCondition(recState)
+	
+// 	public getRecommendations(recState: RecommenderState):
+// 		return self.actionRecommendations
+// }
+ 
+// class Recommendation{
+// 	public Recommendation(action, attributes, buttonText, icon, type):
+// 		self.action = action
+// 		self.attributes = attributes
+// 		self.buttonText = buttonText
+// 		self.icon = icon
+// 		self.type = type
+// 		# ... and whatever else a recommendation needs
